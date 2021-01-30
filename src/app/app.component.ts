b976362c7dd0e6d6129d18c8f7a2be9fa1ae4ef6
Diogo Oliveira <diogo.oliveira@my.istec.pt>
2021-01-30 12:27:15 +0100

    First layout mods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'meuapp';
+  public posts: Post[] = [
+    new Post("João", "Meu Post", "Sub João", "joao@gmail.com", "Minha msg"),
+    new Post("Diogo", "Posto do Diogo", "Sub Diogo", "diogo@gmail.com", "Msg do Diogo"),
+    new Post("Bruno", "Posto do Bruno", "Sub Bruno", "bruno@gmail.com", "Msg do Bruno"),
+    new Post("João", "Meu Post", "Sub João", "joao@gmail.com", "Minha msg"),
+    new Post("Diogo", "Posto do Diogo", "Sub Diogo", "diogo@gmail.com", "Msg do Diogo"),
+    new Post("Bruno", "Posto do Bruno", "Sub Bruno", "bruno@gmail.com", "Msg do Bruno"),
+    new Post("João", "Meu Post", "Sub João", "joao@gmail.com", "Minha msg"),
+    new Post("Diogo", "Posto do Diogo", "Sub Diogo", "diogo@gmail.com", "Msg do Diogo"),
+    new Post("Bruno", "Posto do Bruno", "Sub Bruno", "bruno@gmail.com", "Msg do Bruno"),
+  ];
 }
